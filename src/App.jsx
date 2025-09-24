a863e965da2d2f2eed285d68ecbde4bc169dbc07
TalhaJubaerPrantor <talhajubaer3121@gmail.com>
2025-09-25 01:50:41 +0600

    fixing routing issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
 import Home from './Pages/Home/Home'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import Dashboard from './Pages/Dashboard/Dashboard'
 import Protected from './Components/Utils/Protected'
 import Bookmarks from './Pages/Bookmark/Bookmarks'
+import { useEffect } from 'react'
 
 function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = localStorage.getItem("user"); // or "authToken", etc.
+    if (user) {
+      navigate("/dashboard", { replace: true }); // skip login page
+    }
+  }, [navigate]);
+
   return (
     <>
 
